10d07c097a525ff7c0c3885a665163c755a8be9b
RoyNexus <pacorg@gmail.com>
2016-03-19 00:22:43 +0100

    Examples and use of http module

---
+++ node-examples/simplerect.js
@@ -0,0 +1,22 @@
+var rect = {
+  perimeter: function(x, y) {
+    return (2*(x + y));
+  },
+  area: function(x, y) {
+    return (x * y);
+  }
+};
+
+function solveRect(l, b) {
+  console.log("Solving the rectangle with l = " + l + " and b = " + b);
+  if (l < 0 || b < 0) {
+    console.log("Dimensions should be more than zero");
+  } else {
+    console.log("The area is: " + rect.area(l, b));
+    console.log("The perimeter is: " + rect.perimeter(l, b));
+  }
+}
+
+solveRect(2, 4);
+solveRect(2, 5);
+solveRect(-3, 5);
