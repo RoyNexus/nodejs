77ca6856b487633d33e6d1acef212a203a9446da
RoyNexus <pacorg@gmail.com>
2016-03-24 16:45:26 +0100

    1st assignment finished

--- week1/assignment/server.js
+++ week1/assignment/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var dishRouter = require('./dishRouter');
+var promoRouter = require('./promoRouter');
+var leaderRouter = require('./leaderRouter');
 
 var hostname = 'localhost';
 var port = 3000;
@@ -11,8 +13,12 @@ var app = express();
 app.use(morgan('dev'));
 
 dishRouter.use(bodyParser.json());
+promoRouter.use(bodyParser.json());
+leaderRouter.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
